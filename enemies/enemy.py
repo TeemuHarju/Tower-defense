27ddc26b63d17ba05a282a6049aabfd8d250d46b
TeemuHarju <TeemuHarju@users.noreply.github.com>
2019-12-17 20:07:59 +0200

    Tower game
    game.py and enemy.py started

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,5 +1,5 @@
 import pygame
-
+import math
 
 class Enemy:
     imgs = []
@@ -11,8 +11,12 @@ class Enemy:
         self.height = height
         self.animation_count = 0
         self.health = 1
-        self.path = []
+        self.vel = 3
+        self.path = [(19, 224), (177, 235), (282, 283), (526, 277), (607, 217), (641, 105), (717, 57), (796, 83), (855, 222), (973, 284), (1046, 366), (1022, 458), (894, 502), (740, 514), (580, 552), (148, 551), (85, 452), (52, 345), (1, 335)]
         self.img = None
+        self.path_pos = 0
+        self.move_count = 0
+        self.move_dis = 0
 
     def draw(self, win):
         """
@@ -46,7 +50,28 @@ class Enemy:
         Move enemy
         @return: None
         """
-        pass
+        x1, y1 = self.path[self.path_pos]
+        if self.path_pos + 1 >= len(self.path):
+            x2, y2 = (-10, 355)
+        else:
+            x2, y2 = self.path[self.path_pos + 1]
+
+        move_dis = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+        self.move_count += 1
+        dirn = (x2 - x1, y2 - y1)
+
+        move_x, move_y = (self.x + dir[0] * self.move_count, self.y + dirn[1] * self.move_count)
+        self.dis += math.sqrt((move_x - x1) ** 2 + (move_y - y1) ** 2)
+
+        #got to next point
+        if self.dis >= move_dis:
+            self.dis = 0
+            self.move_count = 0
+            
+
+        self.x = move_x
+        self.y = move_y
 
     def hit(self):
         """
--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import os
 
 class Game:
     def __init__(self):
-        self.width = 1000
+        self.width = 1200
         self.height = 700
         self.win = pygame.display.set_mode((self.width, self.height))
         self.enemys = []
@@ -11,6 +11,7 @@ class Game:
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
 
 
     def run(self):
@@ -19,8 +20,13 @@ class Game:
         while run:
             clock.tick(60)
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
             self.draw()
 
         pygame.quit()
@@ -28,3 +34,6 @@ class Game:
     def draw(self):
         self.win.blit(self.bg, (0,0))
         pygame.display.update()
+        
+g = Game()
+g.run()
