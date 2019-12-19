c044d3fc41b31eee8d1da5666bd35428495faaa7
TeemuHarju <TeemuHarju@users.noreply.github.com>
2019-12-19 17:42:47 +0200

    added clicking for towers

--- towers/tower.py
+++ towers/tower.py
@@ -28,11 +28,12 @@ class Tower():
         win.blit(img, (self.x - img.get_width() // 2, self.y - img.get_height() // 2))
 
     def draw_radious(self, win):
-        # draw change circle
-        surface = pygame.Surface((self.range * 2, self.range * 2), pygame.SRCALPHA, 32)
-        pygame.draw.circle(surface, (128, 128, 128, 100), (self.range, self.range), self.range, 0)
+        if self.selected:
+            # draw change circle
+            surface = pygame.Surface((self.range * 2, self.range * 2), pygame.SRCALPHA, 32)
+            pygame.draw.circle(surface, (128, 128, 128, 100), (self.range, self.range), self.range, 0)
 
-        win.blit(surface, (self.x - self.range, self.y - self.range))
+            win.blit(surface, (self.x - self.range, self.y - self.range))
 
     def click(self, X, Y):
         """
@@ -41,8 +42,9 @@ class Tower():
         @param Y: int
         @return: bool
         """
-        if X <= self.x + self.width and X >= self.x:
-            if Y <= self.y + self.height and Y >= self.y:
+        img = self.tower_imgs[self.level - 1]
+        if X <= self.x  - img.get_width() // 2 + self.width and X >= self.x - img.get_width() // 2:
+            if Y <= self.y + self.height - img.get_height() // 2 and Y >= self.y - img.get_height() // 2:
                 return True
         return False
 
