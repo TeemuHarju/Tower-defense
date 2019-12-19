9c9197b31b7740e5f582ec50027530f80c975612
TeemuHarju <TeemuHarju@users.noreply.github.com>
2019-12-19 21:36:41 +0200

    added upgrade for menus

--- menu/menu.py
+++ menu/menu.py
@@ -2,7 +2,7 @@ import pygame
 import os
 pygame.font.init()
 
-star = pygame.image.load(os.path.join("game_assets", "star.png"))
+star = pygame.transform.scale(pygame.image.load(os.path.join("game_assets", "star.png")), (50, 50))
 
 class Button:
     """
@@ -36,7 +36,7 @@ class Menu:
     """
     menu for holding items
     """
-    def __init__(self, x, y, img, item_cost):
+    def __init__(self, tower, x, y, img, item_cost):
         self.x = x
         self.y = y
         self.width = img.get_width()
@@ -45,7 +45,8 @@ class Menu:
         self.buttons = []
         self.items = 0
         self.bg = img
-        self.font = pygame.font.SysFont("comicsans, 40")
+        self.font = pygame.font.SysFont("comicsans", 25)
+        self.tower = tower
 
     def add_btn(self, img, name):
         """
@@ -68,6 +69,21 @@ class Menu:
         win.blit(self.bg, (self.x - self.bg.get_width() / 2, self.y - 120))
         for item in self.buttons:
             item.draw(win)
+            win.blit(star, (item.x + item.width + 5, item.y - 9))
+            text = self.font.render(str(self.item_cost[self.tower.level - 1]), 1, (255, 255, 255))
+            win.blit(text, (item.x + item.width + 30 - text.get_width() / 2, item.y + star.get_height() - 8))
+
+    def get_clicked(self, X, Y):
+        """
+        return the clicked item from the menu
+        @param X: int
+        @param Y: int
+        @return: str
+        """
+        for btn in self.buttons:
+            if btn.click(X,Y):
+                return btn.name
+        return None
 
     def click(self, X, Y):
         """
@@ -81,14 +97,4 @@ class Menu:
                 return True
         return False
 
-    def get_clicked(self, X, Y):
-        """
-        return the clicked item from the menu
-        @param X: int
-        @param Y: int
-        @return: str
-        """
-        for btn in self.buttons:
-            if btn.click(X,Y):
-                return btn.name
-        return None
+
